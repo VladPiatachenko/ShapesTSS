c04ce859da0362ef94b49ecb947b3859d284cc31
Vlad Piatachenko <vlpiatachenko@gmail.com>
2024-02-12 10:53:20 +0200

    simple tests

--- src/test/java/ShapeTest.java
+++ src/test/java/ShapeTest.java
@@ -7,8 +7,19 @@ public class ShapeTest {
     @Test
     void ShapeByNumberTest(){
         ShapeFactory shape1 = new ShapeFactory(18);
-        Assertions.assertEquals(Color.BLACK,shape1.paint);
+        Assertions.assertEquals(Color.RED,shape1.paint);
         ShapeFactory shape2 = new ShapeFactory(77);
         Assertions.assertEquals(GradientPaint.class, shape2.paint.getClass());
+    }
+    @Test
+    void simpleTest(){
+        Assertions.assertEquals(4,(2+2));
+        Assertions.assertTrue(5>3);
+        Assertions.assertFalse(5<3);
+        Assertions.assertThrows(ArithmeticException.class,()->expression(2,0));
+    }
+
+    int expression(int a, int b)throws ArithmeticException{
+        return a/b;
     }
 }
